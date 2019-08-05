3ba1a616aea35975d0980c553ce8fdf594434897
Varvara Babkova <varvara.munday@gmail.com>
2019-08-05 20:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.querySelector("#main").remove();
 let newHeader = document.createElement("h1");
+newHeader.id = "victory"
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "YOUR-NAME is the champion";
